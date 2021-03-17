91009a54c1e4291be9eb0bdf4337bb213716cb02
bespalov <e.bespalov@propellerads.net>
2021-03-17 07:25:04 +0300

    add some checks to string generator

--- src/app/Task1/RandomStringGenerator.php
+++ src/app/Task1/RandomStringGenerator.php
@@ -11,10 +11,18 @@ class RandomStringGenerator
      */
     public function generate(int $length, $allowedSymbols): string
     {
-        // str_split($allowedSymbols) если строка а если массив то исправить
+        if ($length < 0) {
+            throw new \Exception('The length can\'t be a negative number!');
+        }
+
+        if (!is_array($allowedSymbols) || !is_string($allowedSymbols)) {
+            throw new \Exception('You can pass either array or string as a second parameter');
+        }
+
+        $preparedSymbols = is_array($allowedSymbols) ? $allowedSymbols : str_split($allowedSymbols);
         $result = [];
         for ($i = 1; $length >= $i; $i++) {
-            array_push($result, $allowedSymbols[array_rand(str_split($allowedSymbols), 1)]);
+            array_push($result, $allowedSymbols[array_rand($preparedSymbols, 1)]);
         }
 
         return implode($result);
